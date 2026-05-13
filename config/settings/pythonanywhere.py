953702e8ad9685e0b78b8075cb27b2c3dcb6fc7b
makaveli <ayangbileramadan@gmail.com>
2026-05-13 05:29:44 -0300

    Add pythonanywhere settings (SQLite for free tier)

---
+++ config/settings/pythonanywhere.py
@@ -0,0 +1,15 @@
+from .production import *
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = [os.environ.get('PA_USERNAME', 'youruser') + '.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://' + ALLOWED_HOSTS[0]]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
