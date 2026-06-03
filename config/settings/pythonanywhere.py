b3bb440da2a05fb160d01208ffde22ea4eb1e5b1
makaveli <ayangbileramadan@gmail.com>
2026-06-03 01:24:43 -0300

    fix: hardcode PA domain, add proxy SSL header, disable subdomain HSTS
    pythonanywhere.py was reading PA_USERNAME env var that may not be set,
    defaulting to 'youruser'. Hardcoded to makaveli.pythonanywhere.com.
    Added SECURE_PROXY_SSL_HEADER to prevent redirect loop (PA terminates
    SSL at proxy). Disabled SECURE_HSTS_INCLUDE_SUBDOMAINS to avoid
    breaking all pythonanywhere.com subdomains.

--- config/settings/pythonanywhere.py
+++ config/settings/pythonanywhere.py
@@ -2,8 +2,14 @@ from .production import *
 import os
 
 DEBUG = False
-ALLOWED_HOSTS = [os.environ.get('PA_USERNAME', 'youruser') + '.pythonanywhere.com']
-CSRF_TRUSTED_ORIGINS = ['https://' + ALLOWED_HOSTS[0]]
+ALLOWED_HOSTS = ['makaveli.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://makaveli.pythonanywhere.com']
+
+# Don't HSTS all of pythonanywhere.com
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+
+# PA terminates SSL at proxy, forwards HTTP + X-Forwarded-Proto header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DATABASES = {
     'default': {
