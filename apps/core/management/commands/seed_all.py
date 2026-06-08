28a0bf3c441630543536203274286553371cde5c
makaveli <ayangbileramadan@gmail.com>
2026-06-08 12:44:26 -0300

    Auto-create admin from ADMIN_USERNAME/PASSWORD/EMAIL env vars in seed_all

--- apps/core/management/commands/seed_all.py
+++ apps/core/management/commands/seed_all.py
@@ -1,11 +1,27 @@
+import os
+
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
     help = 'Run all seed commands for demo data'
 
     def handle(self, *args, **options):
+        # Create admin from env vars (set in Render dashboard)
+        admin_user = os.environ.get('ADMIN_USERNAME', '')
+        admin_pass = os.environ.get('ADMIN_PASSWORD', '')
+        admin_email = os.environ.get('ADMIN_EMAIL', '')
+        if admin_user and admin_pass and admin_email:
+            if not User.objects.filter(username=admin_user).exists():
+                User.objects.create_superuser(admin_user, admin_email, admin_pass)
+                self.stdout.write(f'Created admin: {admin_user}')
+            else:
+                self.stdout.write(f'Admin {admin_user} already exists')
+
         self.stdout.write('Seeding favorites...')
         try:
             call_command('seed_favorites', '--users', '3')
